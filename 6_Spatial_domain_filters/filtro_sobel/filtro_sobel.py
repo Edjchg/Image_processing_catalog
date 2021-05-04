c2e8a738e64f3a9ed0744af9c25719179b4bfd69
Edgar Chaves <edjchg@gmail.com>
2021-05-03 22:04:38 -0600

    Add doc to Sobel filter

--- 6_Spatial_domain_filters/filtro_sobel/filtro_sobel.py
+++ 6_Spatial_domain_filters/filtro_sobel/filtro_sobel.py
@@ -2,22 +2,28 @@ import matplotlib.pyplot as plt
 import imageio
 from scipy import signal
 import numpy as np
-
-
 def aplicar_filtro_sobel(imagen_str):
+    # Esta función toma una imagen y aplica sobre esta la convolución con
+    # el kernel tipico del filtro Laplaciano.
+    # Entradas: - imagen_str: la ruta de la imagen deseada.
+    # Salidas: la imagen con la convolución aplicada.
+    # Leyendo la imagen.
     A = imageio.imread(imagen_str)
-
+    # Tomando el kernel para filtrar los bordes horizontales.
     Bx = [[-1, -2, -1],
           [0, 0, 0],
           [1, 2, 1]]
-
+    # Tomando el kernel para filtrar los bordes verticales.
     By = [[-1, 0, 1],
           [-2, 0, 2],
           [-1, 0, 1]]
-
+    # Aplicando el filtro de bordes horizontales.
     Cx = signal.convolve2d(A, Bx)
+    # Aplicando el filtro de bordes verticales.
     Cy = signal.convolve2d(A, By)
+    # Obteniendo las distancias entre los bordes horizontales y verticales.
     C = np.sqrt(np.square(Cx) + np.square(Cy))
+    # Convirtiendo la imagen resultante a tipo uint8.
     C = C.astype(np.uint8)
     return C
 
