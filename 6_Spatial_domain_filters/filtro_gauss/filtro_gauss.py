298b0a4287e24821b42659bcffdc372bbccee667
Edgar Chaves <edjchg@gmail.com>
2021-05-03 21:54:29 -0600

    Add doc to Gaussian filter

--- 6_Spatial_domain_filters/filtro_gauss/filtro_gauss.py
+++ 6_Spatial_domain_filters/filtro_gauss/filtro_gauss.py
@@ -2,15 +2,19 @@ import matplotlib.pyplot as plt
 import imageio
 from scipy import ndimage
 import numpy as np
-
-
 def aplicar_filtro_gauss(imagen_str):
+    # Esta función toma una imagen y aplica sobre esta la convolución con
+    # el kernel tipico del filtro gaussiano.
+    # Entradas: - imagen_str: la ruta de la imagen deseada.
+    # Salidas: la imagen con la convolución aplicada.
+    # Leyendo la imagen.
     A = imageio.imread(imagen_str)
+    # Calculando el kernel tipico del filtro de Gauss.
     B = (1/16)*np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
-    print(B)
+    # Aplicando la convolución a las matrices de A y B.
     C = ndimage.convolve(A, B, mode='constant', cval=1.0)
     return C
-
+# Probando el filtro de Gauss.
 A = imageio.imread("boat.jpg")
 B = aplicar_filtro_gauss("boat.jpg")
 plt.figure()
