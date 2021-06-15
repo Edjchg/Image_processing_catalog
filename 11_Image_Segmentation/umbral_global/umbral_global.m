85abaee3829312813c0c4f5c44e6ceda85cb4410
Edgar <edjchg@gmail.com>
2021-06-15 17:00:18 -0600

    Add doc to segmentation and morph ops

--- 11_Image_Segmentation/umbral_global/umbral_global.m
+++ 11_Image_Segmentation/umbral_global/umbral_global.m
@@ -1,4 +1,11 @@
 function B = umbral_global(A, T)
+  % Función que toma una imgen y un valor 
+  % de frontera para segmentar la imagen en dos
+  % tonalidades de gris.
+  % Entradas: - A: una imagen.
+  %           - T: valor de frontera.
+  % Salidas: - imgen segmentada en dos tonalidades
+  % de gris
   A=im2double(A);
   [m n] = size(A);
   B=zeros(m,n);
